8537a580644d678b242859d371c0f6ebd3c5bf81
PalliNaganjaneyulu <81254851+PalliNaganjaneyulu@users.noreply.github.com>
2023-05-19 17:30:30 +0530

    file system class

---
+++ fs.js
@@ -0,0 +1,73 @@
+
+let fs = require("fs")
+
+// console.log(fs);cls
+
+// fs.writeFile('mycode.txt', "this is my data of node \t", (err)=>{
+//     if(err) throw err;
+//     console.log('Task Done')
+// })
+
+// fs.appendFile('mycode.txt', " code from node FS", (err)=>{
+//     if(err) throw err;
+//     console.log('file append Done')
+// })
+
+// fs.readFile('mycode.txt','UTF-8',(err,data) => {
+//     if(err) throw err;
+//     console.log(data)
+// })
+
+// fs.readFileSync('mycode.txt','UTF-8',(err,data) => {
+//     if(err) throw err;
+//     console.log(data)
+// })
+
+// fs.readFile('cities.json','UTF-8',(err,data) => {
+//     if(err) throw err;
+//     console.log(data)
+// })
+
+// fs.readFileSync('cities.json','UTF-8',(err,data) => {
+//     if(err) throw err;
+//     console.log(data)
+// })
+
+// let data = fs.readFileSync('cities.json',{encoding:'utf-8', flag:'r'})
+
+// console.log("sync"+data)
+
+// let data1 = fs.readFileSync('mycode.txt',{encoding:'utf-8', flag:'r'})
+
+// console.log("sync"+data1)
+
+
+// fs.unlink("mycode12.txt", (err)=>{
+//     if(err) throw err
+//     console.log("file deleted")
+// })
+
+// fs.unlink("mycode1.txt", (err)=>{
+//     if(err) throw err
+//     console.log("file deleted")
+// })
+
+fs.rename('mytest.txt','myFile.xml',(err)=>{
+    if(err) throw err
+    console.log('file renamed')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
